9aae26de8e2ad97405905c71fe3af68c8a02ad0e
earlyerin <rin050301@gmail.com>
2025-07-17 19:28:54 +0900

    Feat: Class

--- .vscode/js_study/05.js
+++ .vscode/js_study/05.js
@@ -1,1 +1,17 @@
-//
+//***** 클래스 *****
+
+class Student {
+  name;
+  age;
+
+  //생성자 : 클래스의 인스턴스를 생성할 때 호출되는 메서드
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+//인스턴스 생성
+const newStudent1 = new Student("rin", 21);
+console.log(newStudent1);
+console.log(newStudent1.name);
